e888c864c0a63b7901f49cbc677795d55c186bf2
mhcenic@gmail.com <mhcenic@gmail.com>
2022-09-06 23:38:41 +0200

    add timing decorator

---
+++ decorator/timer.py
@@ -0,0 +1,37 @@
+from functools import wraps
+import time
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} took {total_time:.4f} seconds')
+        return result
+
+    return timeit_wrapper
+
+
+@timeit
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+
+if __name__ == '__main__':
+    fibonacci(5800)
